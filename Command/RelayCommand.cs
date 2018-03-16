3552c7e5023a77d891c3e1655cdce2512cc24e94
Orkad <nicolas.gidon.43@gmail.com>
2018-03-16 22:26:46 +0100

    Rennomage + commantaires

--- Command/RelayCommand.cs
+++ Command/RelayCommand.cs
@@ -5,57 +5,100 @@ using System.Windows.Input;
 
 namespace MvvmClean.Command
 {
+    /// <summary>
+    /// Defini une commande, basé sur System.Windows.Input.ICommand
+    /// </summary>
     public class RelayCommand : ICommand
     {
         readonly Action<object> _execute;
         readonly Predicate<object> _canExecute;
 
-        public RelayCommand(Action<object> execute)
-            : this(execute, null)
+        /// <summary>
+        /// Création d'une instance de la commande avec l'action que doit executer la commande en paramètre
+        /// </summary>
+        /// <param name="actionToExecute">action a executer</param>
+        public RelayCommand(Action<object> actionToExecute)
+            : this(actionToExecute, null)
         {
         }
 
-        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        /// <summary>
+        /// Création d'une instance de la commande action l'action que doit executer la commande en paramètre
+        /// ainsi que le prédicat déterminant si on peut executer cette commande
+        /// </summary>
+        /// <param name="actionToExecute">action a executer</param>
+        /// <param name="canExecute">prédicat déterminant si on peut executer cette commande</param>
+        public RelayCommand(Action<object> actionToExecute, Predicate<object> canExecute)
         {
-            _execute = execute ?? throw new ArgumentNullException("execute");
+            _execute = actionToExecute ?? throw new ArgumentNullException(nameof(actionToExecute));
             _canExecute = canExecute;
         }
 
+        /// <summary>
+        /// Détermine si la commande peut être executée avec le paramètre donné
+        /// </summary>
+        /// <param name="parameter">paramètre d'execution de la commande</param>
+        /// <returns></returns>
         [DebuggerStepThrough]
         public bool CanExecute(object parameter)
         {
             return _canExecute?.Invoke(parameter) ?? true;
         }
 
+        /// <summary>
+        /// evenement se produisant lorsque l'état de la possibilité d'execution change
+        /// </summary>
         public event EventHandler CanExecuteChanged
         {
-            add { CommandManager.RequerySuggested += value; }
-            remove { CommandManager.RequerySuggested -= value; }
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
         }
 
+        /// <summary>
+        /// Execute la commande avec le paramètre donné
+        /// </summary>
+        /// <param name="parameter">paramètre d'execution de la commande</param>
         public void Execute(object parameter)
         {
             _execute(parameter);
         }
     }
 
-
+    /// <summary>
+    /// Defini une commande typée, basé sur System.Windows.Input.ICommand
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public class RelayCommand<T> : ICommand
     {
         readonly Action<T> _action;
         readonly Predicate<T> _canInvokeAction;
 
-        public RelayCommand(Action<T> execute)
-            : this(execute, null)
+        /// <summary>
+        /// Création d'une instance de la commande avec l'action que doit executer la commande en paramètre
+        /// </summary>
+        /// <param name="actionToExecute">action a executer</param>
+        public RelayCommand(Action<T> actionToExecute)
+            : this(actionToExecute, null)
         {
         }
 
-        public RelayCommand(Action<T> execute, Predicate<T> canExecute)
+        /// <summary>
+        /// Création d'une instance de la commande action l'action que doit executer la commande en paramètre
+        /// ainsi que le prédicat déterminant si on peut executer cette commande
+        /// </summary>
+        /// <param name="actionToExecute">action a executer</param>
+        /// <param name="canExecute">prédicat déterminant si on peut executer cette commande</param>
+        public RelayCommand(Action<T> actionToExecute, Predicate<T> canExecute)
         {
-            _action = execute ?? throw new ArgumentNullException(nameof(execute));
+            _action = actionToExecute ?? throw new ArgumentNullException(nameof(actionToExecute));
             _canInvokeAction = canExecute;
         }
 
+        /// <summary>
+        /// Détermine si la commande peut être executée avec le paramètre donné
+        /// </summary>
+        /// <param name="parameter">paramètre d'execution de la commande</param>
+        /// <returns></returns>
         [DebuggerStepThrough]
         public bool CanExecute(object parameter)
         {
@@ -68,12 +111,19 @@ namespace MvvmClean.Command
             return false;
         }
 
+        /// <summary>
+        /// evenement se produisant lorsque l'état de la possibilité d'execution change
+        /// </summary>
         public event EventHandler CanExecuteChanged
         {
             add => CommandManager.RequerySuggested += value;
             remove => CommandManager.RequerySuggested -= value;
         }
 
+        /// <summary>
+        /// Execute la commande avec le paramètre donné
+        /// </summary>
+        /// <param name="parameter">paramètre d'execution de la commande</param>
         public void Execute(object parameter)
         {
             if (typeof(T).IsPrimitive)
